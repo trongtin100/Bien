fb406f30c3b00872249cc36f72453c7562d3492f
Admin <trongtin100@gmail.com>
2022-08-21 08:51:33 +0700

    update-05-Bien-Su-dungToanTu

---
+++ EX-07-04-BienSuDungToanTu.js
@@ -0,0 +1,7 @@
+tinWidth = prompt ("Enter the width");
+tinHeight= prompt ("Enter the height");
+
+let width = parseInt(tinWidth);
+let height = parseInt(tinHeight);
+let area = width * height ;
+    document.write("The area is: " + area )
